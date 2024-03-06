0fa2109018109a313b46e4922d36e66ab0bd3380
Evelyn <evelyn.vanielly@gmail.com>
2024-03-06 15:51:47 -0300

    feature-layout-style

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
