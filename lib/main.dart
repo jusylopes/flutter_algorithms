0be3b9322378d3ccc96c3f835ed7b4a305e9ec89
petronio50 <petronioeoliveira@hotmail.com>
2024-03-06 22:09:16 -0300

    refactor:changing the color of the top bar purple from to blue

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.blue,
         title: Text(widget.title),
       ),
       body: Center(
