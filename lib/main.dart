142f2037b4790978e83c302ce74b2ec9e6b2952d
Evelyn <evelyn.vanielly@gmail.com>
2024-03-16 16:55:44 -0300

    feat: create challenge 3

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,19 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+void _desafio3() {
+    int result = 1;
+    int fatorial = 5;
+
+    for (int i = 1; i <= fatorial; i++) {
+      result *= i;
+    }
+
+    setState(() {
+      _result = 'O resultado fatorial de $fatorial! é $result.';
+    });
+  }
+
   void _desafio4(int number) {
     String evenOrOdd = number % 2 == 0 ? 'par' : 'ímpar';
     String positiveOrNegative = number > 0 ? 'positivo' : 'negativo';
