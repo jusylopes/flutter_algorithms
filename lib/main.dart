d4e533b1831fe9e94a218248affb8a32b8448f9c
AlexandreCalmonJr <alexandrecalmonjunior@gmail.com>
2024-03-17 16:56:13 -0700

    feat: create challenge 7

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-   void _desafio8(valueA, valueB, valueC) {//resolução do desafio 8
+  void _desafio7(double userSalary, double minimumSalary){ // resolução do desafio 15
+    double salaryMultiplier = userSalary / minimumSalary;
+  setState(() {
+    _result = 'O usuário recebe um salário igual a ${salaryMultiplier.toStringAsFixed(2)} vezes o salário mínimo.';
+  });
+ }
+
+  void _desafio8(valueA, valueB, valueC) {//resolução do desafio 8
 
     List<int> listNumber = [valueA, valueB, valueC];
 
@@ -71,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _result = list.toString() + descending.toString();
     });  
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -95,7 +103,8 @@ class _MyHomePageState extends State<MyHomePage> {
               callChallenge: () {
                 // _desafio2(2, 5, 10);
                 //_desafio4(-7);
-                _desafio8(57,7,99);
+                //_desafio8(57,7,99);
+                _desafio7(1871, 1417);
               },
             ),
             const SizedBox(height: 20),
